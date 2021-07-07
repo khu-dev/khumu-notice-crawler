ad8b2eaaf640b2653085c003e9fba4afb3aea12e
Songchihoon <clgns0415@gmail.com>
2021-07-07 21:26:57 +0900

    add jparepository

---
+++ src/main/java/khumu/spring/batch/repository/WebUrlRepository.java
@@ -0,0 +1,7 @@
+package khumu.spring.batch.repository;
+
+import khumu.spring.batch.dto.WebUrl;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WebUrlRepository extends JpaRepository<WebUrl, Long> {
+}
