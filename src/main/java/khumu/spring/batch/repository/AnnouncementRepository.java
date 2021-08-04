12b084d07b42e4a25d172a5da153e4032ab17ddf
Marhead <clgns0415@gmail.com>
2021-08-04 23:34:59 +0900

    Update AnnouncementRepository.java

--- src/main/java/khumu/spring/batch/repository/AnnouncementRepository.java
+++ src/main/java/khumu/spring/batch/repository/AnnouncementRepository.java
@@ -2,6 +2,15 @@ package khumu.spring.batch.repository;
 
 import khumu.spring.batch.data.entity.Announcement;
 import org.springframework.data.jpa.repository.JpaRepository;
-@RepositoryRestResource
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRe1 = "announcement", path = "announcement")
 public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
+    List<Announcement> findByName(@Param("author") String name);
+
+    List<Announcement> findByNameStartsWithIgnoreCase(String name);
+
+    List<Announcement> findByNameAndDescription(String name, String description);
 }
