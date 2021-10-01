e95f52893fbb71b272ae0c609aa911d23235c178
Marhead <clgns0415@gmail.com>
2021-10-02 00:23:57 +0900

    Update app.yaml

--- src/main/java/khumu/spring/batch/tasklet/SWBoardCrawling.java
+++ src/main/java/khumu/spring/batch/tasklet/SWBoardCrawling.java
@@ -13,10 +13,8 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.EntityManagerFactory;
 import java.util.ArrayList;
 import java.util.List;
 
